093581205b139c0207f139b2c49079117d9d6e14
Antoine Parise <antoine.parise@mail.mcgill.ca>
2025-10-29 23:09:00 -0400

    mcts done untested

--- agents/student_agent.py
+++ agents/student_agent.py
@@ -7,6 +7,9 @@ from copy import deepcopy
 import time
 from helpers import random_move, execute_move, check_endgame, get_valid_moves
 
+# random_pool = np.random.randint(0, 48, size=10_000)
+
+
 @register_agent("student_agent")
 class StudentAgent(Agent):
   """
@@ -20,6 +23,26 @@ class StudentAgent(Agent):
     
     self.random_pool = np.random.randint(0, 48, size=10_000)
 
+  def mcts_search(self, root_state, player, iter=500):
+    root = MCTSNode(root_state, player, minmax=0)
+
+    for _ in range(iter):
+      node = root
+
+      while not node.is_terminal() and node.is_fully_expanded():
+            node = node.best_child()
+
+      if not node.is_terminal():
+        node = node.expand()
+
+          # Simulation
+      result = node.rollout()
+
+      # Backpropagation
+      node.backpropagate(result)
+
+    return root.best_child(c=0).action
+      
     
   def step(self, chess_board, player, opponent):
     """
@@ -51,50 +74,98 @@ class StudentAgent(Agent):
     return random_move(chess_board,player)
 
 class MCTSNode:
-  def __init__(self, state, player=0, parent=None, action=None):
+  def __init__(self, state, player=1, minmax=0, parent=None, action=None, rng=np.random.default_rng()):
     self.state = state
     self.parent = parent
     self.action = action
     self.children = []
 
     self.player = player
+    self.minmax = minmax #0 for max 1 for min players
 
     self.visits = 0
     self.wins  = 0
+
+    self.rng = rng
 
     #ratio of friendly vs hostile tiles may prove usefull
     self.ratio = 0.5
     self.untried_action = get_valid_moves(self.state, player)
 
-    def is_terminal(self):
-      is_endgame = False
-
-      if np.sum(self.state == 0) == 0:
-          is_endgame = True
-      
-      return is_endgame
+  def is_terminal(self):
+    is_endgame = False
+    if np.sum(self.state == 0) == 0:
+        is_endgame = True
     
-    def is_fully_expanded(self):
-      return len(self.untried_action) == 0  
+    return is_endgame
+  
+  def is_fully_expanded(self):
+    return len(self.untried_action) == 0  
 
 
-    def expand(self):
-      #consider switching to the queue library to make this efficient
-      action = self.untried_actions.pop()
-      new_state = execute_move(self.state, action, self.player)
-      
-      #flip player
-      new_player = self.player ^ 1
+  def expand(self):
+    """
+    Expand new child node
+    """
+    #consider switching to the queue library to make this efficient
+    action = self.untried_action.pop()
+    new_state = execute_move(self.state, action, self.player)
+    
+    #flip player
+    new_player = 3 - self.player 
+    minmax = 1 - self.minmax
+    child = MCTSNode(new_state, player=new_player, minmax=minmax, parent=self, action=action, rng=self.rng) 
+    return child
 
-      child = MCTSNode(new_state, player= new_player, parent=self, action=action)
-      return child
+  def best_child(self, c=1.4):
+    """
+    Return best child using upper confidence tree comparison.  
+    """
+    if self.minmax == 0: #max player
+      return max(self.children, key=lambda child: self.UCB1(child, c))
+    else:
+      return min(self.children, key=lambda child: self.UCB1(child, -c))
 
-    def best_child(self):
-      pass
         
+  def UCB1(self, child, c):
+    return (child.wins/child.visits) + c * np.sqrt(np.log(self.visits)/child.visits)
+  
+  def rollout(self):
+    """
+    Simulate game until completion 
+    """
+    curr_state = np.copy(self.state)
+    curr_player = self.player
+    while True:
+      allowed_moves = get_valid_moves(curr_state, curr_player)
+      number_allowed_moves = len(allowed_moves)
 
-    def UCB1(self, c=1.4):
-      pass
+      #grab a random move
+      move = allowed_moves[self.rng.integers(0, number_allowed_moves)]
+
+      curr_state = execute_move(curr_state, move, curr_player)
+      is_endgame, p0, p1 = check_endgame(curr_state)
+      if is_endgame:
+        if p0 > p1: return 1
+        if p0 < p1: return -1
+        else: return 0
+      curr_player = 3 - curr_player
+
+  def backpropagate(self, result):
+    """
+    Update tree
+    """
+    self.visits += 1
+    self.wins += result
+    
+    if self.parent:
+      self.parent.backpropagate(result)
+
+
+
+
+
+
 
 
 
