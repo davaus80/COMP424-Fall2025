324064209eeffcd96f5f0aa27d0f29c8dfc1c1e4
tonynokart <thomasson5leo@gmail.com>
2025-11-13 15:10:50 -0500

    basic  minimax (beats greedy search) unefficient

--- agents/student_agent.py
+++ agents/student_agent.py
@@ -1,5 +1,5 @@
 # Student agent: Add your own agent here
-# python simulator.py --player_1 greedy_corners_agent --player_2 random_agent --display
+# python simulator.py --player_1 greedy_corners_agent --player_2 student_agent --display
 
 from agents.agent import Agent
 from store import register_agent
@@ -41,11 +41,68 @@ class StudentAgent(Agent):
     # time_taken during your search and breaking with the best answer
     # so far when it nears 2 seconds.
     start_time = time.time()
-    time_taken = time.time() - start_time
 
-    print("My AI's turn took ", time_taken, "seconds.")
+    # statistics
+    avg_calc_time = 0
+    n = 0
+
+    
+    valid_moves = get_valid_moves(chess_board, player)
+    bestMove = None
+    bestValue = -10000
+
+    # MINIMAX ALGORITHM 
+    for move in valid_moves:
+      sim_board = deepcopy(chess_board)
+      execute_move(sim_board, move, player)
+
+      value = self.minimax(sim_board, 2, False, player, opponent)
+
+      if (value > bestValue):
+        bestValue = value
+        bestMove = move
+
+    
+      
+    if (bestMove != None):
+      print("\t\tfrom: (", bestMove.row_src ,", ", bestMove.col_src,") --> to: (", bestMove.row_dest ,", ", bestMove.col_dest,")")
+
+
+    time_taken = time.time() - start_time
+    print("\t\tMy AI's turn took ", time_taken, "seconds.")
 
     # Dummy return (you should replace this with your actual logic)
     # Returning a random valid move as an example
-    return random_move(chess_board,player)
+    return bestMove or random_move
+  
+  def calculate_heuristic(self, board, player, opponent):
+    plyr_count = np.count_nonzero(board == player)
+    opp_count = np.count_nonzero(board == opponent)
+
+    # print("\t\t h: ", plyr_count - opp_count, " - ", plyr_count," - ",  opp_count)
+    return plyr_count - opp_count
+  
+  def minimax(self, board_state, depth, MAX, plyr, opp):
+    if depth == 0 or check_endgame(board_state) == True:
+      return self.calculate_heuristic(board_state, plyr, opp) 
+
+    if MAX:
+      value = -10000
+      legal_moves = get_valid_moves(board_state, plyr)
+      for move in legal_moves:
+        sim_board = deepcopy(board_state)
+        execute_move(sim_board, move, plyr)
+        value = max(value, self.minimax(sim_board, depth-1, False, plyr, opp))
+        # print(" ---+++---max: ",value)
+    else:
+      value = 10000
+      legal_moves = get_valid_moves(board_state, opp)
+      for move in legal_moves:
+        sim_board = deepcopy(board_state)
+        execute_move(sim_board, move, opp)
+        value = min(value, self.minimax(sim_board, depth-1, True, plyr, opp))
+        # print(" ---+++---min: ",value)
+
+    return value
+
 
